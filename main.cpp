b7cedb0f9d93cc45405a4c4a8b30c2914622dc19
Vítor Plentz <vrplentz@inf.ufpel.edu.br>
2017-07-07 13:57:31 -0300

    Double Linked List; Hash with inner functions; Rehashing; Remove working

--- main.cpp
+++ main.cpp
@@ -6,16 +6,21 @@ class Node {
     private:
         string word;
         Node * next = NULL;
+        Node * previusly = NULL;
     public:
     Node(){
         this->next = NULL;
+        this->previusly = NULL;
         this->word = "";
     }
-    Node(string word, Node *next) {
+    Node(string word, Node *next, Node * previusly) {
         this->next = next;
         this->word = word;
+        this->previusly = previusly;
     }
-
+    Node * getPreviusly(){
+        return  this->previusly;
+    }
     string getWord() {
         return this->word;
     }
@@ -26,7 +31,10 @@ class Node {
     void setWord(string word) {
         this->word = word;
     }
-    Node setNext(Node * next) {
+    void setPreviusly(Node * previusly){
+        this->previusly = previusly;
+    }
+    void setNext(Node * next) {
         this->next = next;
     }
 
@@ -38,6 +46,11 @@ class Hash{
         int numOfElements;
         int tableSize;
         double loadFactor; //numOfElements/tableSize
+
+    int hashF(string input){
+        int inputInt = (int) input.size();
+        return inputInt%this->tableSize;
+    }
     public:
 
     Hash(double loadFactor, int tableSize) {
@@ -73,86 +86,139 @@ class Hash{
     double  setLoadFactor(double loadFactor){
         this->loadFactor = loadFactor;
     }
+    void remove(string input){
+        int hashedto = this->hashF(input);
+        for(Node * node = this->table[hashedto]; node != NULL; node = node->getNext()){
+            if(node->getWord().compare(input) == 0){
+                if(node == this->table[hashedto]){
+                    Node * aux = node->getNext();
+                    if(node->getNext()!= NULL)
+                        node->getNext()->setPreviusly(NULL);
+                    delete node;
+                    this->table[hashedto] = aux;
+                    this->numOfElements--;
+                    cout << "ok\n";
+                    return;
+                }
+                if(node->getPreviusly() != NULL)
+                    node->getPreviusly()->setNext(node->getNext());
+                if(node->getNext() != NULL)
+                    node->getNext()->setPreviusly(node->getPreviusly());
+                delete node;
+                this->numOfElements--;
+                cout << "ok\n";
+                return;
+            }
+        }
+        cout << "fail\n";
+    }
+    void put(string input){
+        int hashedTo = hashF(input);
+        if (this->table[hashedTo] == NULL) {
+            Node *newNode = new Node(input, NULL, NULL);
+            this->table[hashedTo] = newNode;
+        }else {
+            Node *newNode = new Node(input, this->getTable(hashedTo), NULL);
+            this->table[hashedTo] = newNode;
+            this->table[hashedTo]->getNext()->setPreviusly(newNode);
+        }
+        this->numOfElements++;
+        cout << "ok\n";
+        if(((double)this->numOfElements/(double)this->tableSize) > this->loadFactor){
+            cout<< "should rehash";
+            rehash();
+            printHash();
+        }
 
+    }
+    bool containsValue(string value){
+        int hashedto = hashF(value);
+        for(Node * node = this->table[hashedto]; node != NULL; node = node->getNext()){
+            if(node->getWord().compare(value) == 0)
+                return true;
+        }
+        return false;
+    }
+    void clear(){
+        Node * aux = NULL;
+        for(int i = 0; i < this->tableSize; i++){
+            for(Node * node = this->table[i]; node != NULL; node = aux){
+                aux = node->getNext();
+                delete node;
+            }
+        }
+        delete[] this->table;
+    };
+    void rehash(){
+        int newSize =  this->tableSize * 2;
+        Hash *newHashTable = new Hash(0.75, newSize);
+        Node * aux = NULL;
+        for(int i = 0; i < this->tableSize; i++){
+            for(Node * node = this->table[i]; node != NULL; node = aux){
+                newHashTable->put(node->getWord());
+                aux = node->getNext();
+                delete node;
+            }
+        }
+        delete[] this->table;
+        this->tableSize = newSize;
+        this->table = newHashTable->table;
+        delete newHashTable;
+    }
+    void printHash(){
+        for(int i = 0; i < this->tableSize; i++){
+            for(Node * node = this->table[i]; node != NULL; node = node->getNext()){
+                cout << "\n position ";
+                cout << i;
+                cout << " word: ";
+                cout << node->getWord();
+            }
+        }
+        /*Node * node;
+        for(int i = 0; i < this->tableSize; i++){
+            for(node = this->table[i]; node != NULL; node = node->getNext())
+                if(node->getNext() == NULL)
+                    break;
+            for(Node * node1 = node; node1 != NULL; node1 = node1->getPreviusly()){
+                cout << "\n position ";
+                cout << i;
+                cout << " word: ";
+                cout << node1->getWord();
+            }
+        }*/
+
+    }
 };
 
-int hashF(string input, int tableSize);
-
-void printHash(Hash * hashTable);
-
-void freeHash(Hash * hashTable);
-
-bool verifyWord(Hash * hashTable, string input);
 
 int main() {
     string inputWord, lastInputWord;
-    Hash *hashTable = new Hash(0.5, 5);
+    Hash *hashTable = new Hash(0.75, 5);
     while(cin >> inputWord){
         if(inputWord.compare("+") == 0){
-            if (lastInputWord.empty() || verifyWord(hashTable, lastInputWord))
-                cout << "fail+";
-            else {
-                int hashedTo = hashF(lastInputWord, hashTable->getTableSize());
-                if (hashTable->getTable(hashedTo) == NULL) {
-                    Node *newNode = new Node(lastInputWord, NULL);
-                    hashTable->setTable(hashedTo, newNode);
-                } else {
-                    Node *newNode = new Node(lastInputWord, hashTable->getTable(hashedTo));
-                    hashTable->setTable(hashedTo, newNode);
-                }
-                hashTable->setNumOfElements(hashTable->getNumOfElements()+1);
-                cout << "ok";
-                if(((double)hashTable->getNumOfElements()/(double)hashTable->getTableSize()) > hashTable->getLoadFactor())
-                    cout<< "should rehash";
-            }
+            if (lastInputWord.empty() || hashTable->containsValue(lastInputWord))
+                cout << "fail\n";
+            else
+                hashTable->put(lastInputWord);
         }else if(inputWord.compare("-") == 0){
             if(lastInputWord.empty())
-                cout << "fail-";
-            else
-                cout << "-" + lastInputWord;
+                cout << "fail\n";
+            else {
+                hashTable->remove(lastInputWord);
+            }
         }else if (inputWord.compare("*") == 0){
-            freeHash(hashTable);
+            hashTable->clear();
+            delete hashTable;
             break;
         }else if(inputWord.compare("?") == 0){
-            printHash(hashTable);
+            hashTable->printHash();
         }else{//find word
             lastInputWord = inputWord;
-            if(verifyWord(hashTable, lastInputWord))
-                cout << "ok";
+            if(hashTable->containsValue(lastInputWord))
+                cout << "ok\n";
         }
     }
     return 0;
 }
-int hashF(string input, int tableSize){
-    int inputInt = (int) input.size();
-    return inputInt%tableSize;
-}
-void printHash(Hash * hashTable){
-    for(int i = 0; i < hashTable->getTableSize(); i++){
-        for(Node * node = hashTable->getTable(i); node != NULL; node = node->getNext()){
-            cout << "\n position ";
-            cout << i;
-            cout << " word: ";
-            cout << node->getWord();
-        }
-    }
-}
-bool verifyWord(Hash * hashTable, string input){
-    int hashedto = hashF(input, hashTable->getTableSize());
-    for(Node * node = hashTable->getTable(hashedto); node != NULL; node = node->getNext()){
-        if(node->getWord().compare(input) == 0)
-            return true;
-    }
-    return false;
-}
-void freeHash(Hash * hashTable){
-    Node * aux = NULL;
-    for(int i = 0; i < hashTable->getTableSize(); i++){
-        for(Node * node = hashTable->getTable(i); node != NULL; node = aux){
-            aux = node->getNext();
-            delete node;
-        }
-    }
-    delete[] hashTable->getTable();
-    delete hashTable;
-}
+
+
